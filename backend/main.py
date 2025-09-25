a15127ea7d5496c01d987ebabe9288e8aec72ffe
Ruhannn <ruhanrouf17@gmail.com>
2025-09-26 03:04:49 +0600

    backend added

--- backend/main.py
+++ backend/main.py
@@ -13,8 +13,8 @@ handle_bus_routes(app)
 handle_location_routes(app)
 handle_route_routes(app)
 
-@app.get('/')
-def index():
-    return "Bus Khoja API is running."
+@app.get("/")
+def route(req):
+    req.send(204, "Bus Khoja API is running.")
 
 app.run()
